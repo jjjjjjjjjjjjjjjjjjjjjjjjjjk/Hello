feeea0be4e1e188ad0bac24bb49c613cae827f59
jjjjjjjjjjjjjjjjjjjjjjjjjjk <80856838+jjjjjjjjjjjjjjjjjjjjjjjjjjk@users.noreply.github.com>
2022-06-05 15:31:35 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,4 +2,4 @@ print (Ransomware v2 to decrypt please wait for further instruction);
 print (your files are safe for a little while, but no for long);
 import time
 time.sleep(3)
-print (EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE);
+print (EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE)
