517e9cd465fde7fb171bab1bd6667acc128b1ba3
jjjjjjjjjjjjjjjjjjjjjjjjjjk <80856838+jjjjjjjjjjjjjjjjjjjjjjjjjjk@users.noreply.github.com>
2022-06-05 14:58:00 -0400

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.Clear();
+Console.WriteLine("Hello, World!");
+
+NewMethod();
+Console.WriteLine(value: "Ransomware v2 to decrypt please wait for further instruction...");
+
+static void NewMethod()
+{
+    Console.BackgroundColor = ConsoleColor.Blue;
+}
